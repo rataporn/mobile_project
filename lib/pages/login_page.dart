df59e3a6eb65512dd90afb20be1c28e6c6db1ce4
rataporn <rataporn.ph@ku.th>
2024-03-30 21:09:34 +0700

    Add Sign in with Google

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:twitty/components/button.dart';
 import 'package:twitty/components/text_field.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:twitty/pages/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   final Function()? onTap;
@@ -84,7 +86,7 @@ class _LoginPageState extends State<LoginPage> {
                 // welcome
                 Text(
                   "Welcome To Twitty!",
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.white, fontSize: 25),
                 ),
                 const SizedBox(height: 25),
 
@@ -109,7 +111,7 @@ class _LoginPageState extends State<LoginPage> {
                   onTap: signIn,
                   text: 'Sign In',
                 ),
-                const SizedBox(height: 25),
+                const SizedBox(height: 15),
 
                 // go to register page
                 Row(
@@ -134,11 +136,74 @@ class _LoginPageState extends State<LoginPage> {
                     )
                   ],
                 ),
+                const SizedBox(height: 10),
+                Text("or"),
+                const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () async {
+                    var userCredential = await signInWithGoogle();
+                    if (userCredential != null) {
+                      print(userCredential.toString());
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              HomePage(), // Navigate to your home page
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    height: 48,
+                    width: 220,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.12),
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/google_logo.png',
+                          height: 18,
+                          width: 18,
+                        ),
+                        SizedBox(width: 14),
+                        Text(
+                          'Sign in with Google',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Future<UserCredential?> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
